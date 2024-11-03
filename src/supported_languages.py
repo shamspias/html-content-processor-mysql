300c52ce750d2d6521db4f5d2253c433288ed75b
Shamsuddin Ahmed <shamsuddinpias0@gmail.com>
2024-11-03 14:27:44 +0600

    add support language

---
+++ src/supported_languages.py
@@ -0,0 +1,21 @@
+# supported_languages.py
+
+supported_langs = {
+    'ar': 'arabic',
+    'bn': 'bangla',
+    'da': 'danish',
+    'nl': 'dutch',
+    'en': 'english',
+    'fi': 'finnish',
+    'fr': 'french',
+    'de': 'german',
+    'el': 'greek',
+    'it': 'italian',
+    'no': 'norwegian',
+    'pt': 'portuguese',
+    'es': 'spanish',
+    'sv': 'swedish',
+    'tr': 'turkish',
+    'ru': 'russian',  # Added Russian language support
+    # Add more mappings as needed
+}
